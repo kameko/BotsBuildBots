92a361bb3288961507b54968fcbca18648d9c66d
Андрей <andwebar@gmail.com>
2015-10-31 01:58:03 +0500

    add JS

---
+++ botsbuildbots.js
@@ -0,0 +1,13 @@
+'use strict';
+
+var c = console;
+
+function botbuildbots() {
+    try {
+        botbuildbots();
+    } catch(e) {
+        c.log('all out of bots/stack frames :[');            
+    }
+}
+
+botbuildbots();
